e7dfe10bfd3c29df7987f427dc8cbc1b946e03c8
ashok <sunuwarashok99@gmail.com>
2023-07-28 21:52:15 +0545

    initial commit

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="admin"
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE IF NOT EXISTS elderco")
+
+print("Database created successfully")
